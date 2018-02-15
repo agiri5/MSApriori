f142cbb49e7a11bb87f078aac364d2768ca0ec8f
Kashyap Desai <35252559+kashyap9395@users.noreply.github.com>
2018-02-14 20:18:58 -0600

    Update ms_apriori.py

--- ms_apriori.py
+++ ms_apriori.py
@@ -1,2 +1,14 @@
 #CS 583 Project 1 - MS APriori Algorithm 
 #Contributors: Ayush and Kashyap
+
+T = []
+
+def readInput():
+    ''' reads the input transactions from the file '''
+    
+    f = open('input-data.txt','r')
+
+    for line in f.readlines():
+        transaction = line.strip().replace('{','').replace('}','').split(',')
+        transaction = [int(x) for x in transaction]
+        T.append(transaction)
