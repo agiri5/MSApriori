e0a0a28dcbe334d93f73a3aaebb97bde064a7819
Kashyap Desai <35252559+kashyap9395@users.noreply.github.com>
2018-02-14 21:11:55 -0600

    Update ms_apriori.py

--- ms_apriori.py
+++ ms_apriori.py
@@ -2,6 +2,9 @@
 #Contributors: Ayush and Kashyap
 
 T = []
+MS = {} 
+illegal = []
+must = []
 
 def readInput():
     ''' reads the input transactions from the file '''
@@ -12,3 +15,25 @@ def readInput():
         transaction = line.strip().replace('{','').replace('}','').split(',')
         transaction = [int(x) for x in transaction]
         T.append(transaction)
+
+def readParams():
+    f = open('parameter-file.txt','r')
+
+    for line in f.readlines():
+        if line.find("MIS") != -1:
+            temp = line.strip().replace('MIS','').replace('(','').replace(')','').split('=')
+            MS[int(temp[0])] = float(temp[1])
+        
+        if line.find("SDC") != -1:
+            phi = float(line.replace(' ','').rstrip().split('=')[1])
+
+        if line.find("cannot_be_together") != -1:
+            cannot = line.strip().replace('cannot_be_together','').replace(':','').replace('{','').split('},')
+            for i in cannot: 
+                grp = i.strip().replace('}','').split(',')
+                grp = [int(x) for x in grp]
+                illegal.append(grp)
+        
+        if line.find("must-have") != -1:
+            have = line.strip().replace('must-have','').replace(':','').split('or')
+            must = [int(x) for x in have]        
